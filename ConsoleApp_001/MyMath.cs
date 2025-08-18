02667c93aa000b3282559b3a17c294566ef99786
Nazar Tobilevych <nz.tobilevych@gmail.com>
2025-08-18 09:37:21 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp_001/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_001
+{
+    internal class MyMath
+    {
+        static internal int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static internal int Substract(int c, int d)
+        { 
+            return c - d; 
+        }
+    }
+}
---
+++ ConsoleApp_001/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 2;
+            int b = 3;
+            int summe = MyMath.Add(a, b);
+            Console.WriteLine($"Summe von {a} und {b} = {summe}");
+
+            int c = 6;
+            int d = 7;
+            int resultat = MyMath.Substract(c, d);
+            Console.WriteLine($"Resultat von {c} minus {d} = {resultat}");
+        }
+    }
+}

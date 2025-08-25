1276d870591112be56229eed40473c9f499951df
nazar813 <nz.tobilevych@gmail.com>
2025-08-25 08:30:02 +0200

    Add files via upload

---
+++ Aufgabe_ggt_kgv/Aufgabe_ggt_kgv/MyMath.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Aufgabe_ggt_kgv
+{
+    internal class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            while (b != 0)
+            {
+                int rest = a % b;
+                a = b;
+                b = rest;
+            }
+            return a;
+        }
+
+        public static int Calc_kgV(int a, int b)
+        {
+            return (a * b) / Calc_ggT(a, b);
+        }
+
+        public static int ReadInt(string text)
+        {
+            int zahl;
+            do
+            {
+                Console.Write(text + ": ");
+                string eingabe = Console.ReadLine();
+
+                if (int.TryParse(eingabe, out zahl) && zahl > 0)
+                {
+                    return zahl;
+                }
+                else
+                {
+                    Console.WriteLine("Bitte eine positive ganze Zahl eingeben!");
+                }
+            } while (true);
+        }
+
+        public static void ShowResult(string type, int a, int b, int result)
+        {
+            Console.WriteLine($"{type} von {a} und {b} ist {result}");
+        }
+    }
+}
---
+++ Aufgabe_ggt_kgv/Aufgabe_ggt_kgv/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+namespace DeinProjektname
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool weiter = true;
+
+            while (weiter)
+            {
+                Console.WriteLine("\n=== Menü ===");
+                Console.WriteLine("1 = ggT berechnen");
+                Console.WriteLine("2 = kgV berechnen");
+                Console.WriteLine("0 = Ende");
+                Console.Write("Deine Auswahl: ");
+                string wahl = Console.ReadLine();
+
+                if (wahl == "0")
+                {
+                    weiter = false; // Programm beenden
+                }
+                else if (wahl == "1" || wahl == "2")
+                {
+                    int a = MyMath.ReadInt("Gib die erste Zahl ein");
+                    int b = MyMath.ReadInt("Gib die zweite Zahl ein");
+
+                    if (wahl == "1")
+                    {
+                        int ggt = MyMath.Calc_ggT(a, b);
+                        MyMath.ShowResult("ggT", a, b, ggt);
+                    }
+                    else if (wahl == "2")
+                    {
+                        int kgv = MyMath.Calc_kgV(a, b);
+                        MyMath.ShowResult("kgV", a, b, kgv);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe, bitte wähle 0, 1 oder 2!");
+                }
+            }
+
+            Console.WriteLine("Programm beendet.");
+        }
+    }
+}
